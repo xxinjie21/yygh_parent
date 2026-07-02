0695067d242a4881862793b6f0d310493f420037
xxinj <xxinjie21@163.com>
2026-07-02 18:15:38 +0800

    fix: ApiServiceImpl.getImageBytes() 异常处理修复 - 删除重复的 getInputStream() 调用（资源泄漏） - 添加 MalformedURLException import + catch - 添加 IllegalArgumentException catch（URI.create()） - 异常不再静默吞掉，改为 throw YyghException - 删除不可达的 return null

--- hospital-manage/src/main/java/com/yygh/hospital/service/impl/ApiServiceImpl.java
+++ hospital-manage/src/main/java/com/yygh/hospital/service/impl/ApiServiceImpl.java
@@ -7,16 +7,15 @@ import com.yygh.hospital.mapper.ScheduleMapper;
 import com.yygh.hospital.model.HospitalSet;
 import com.yygh.hospital.model.Schedule;
 import com.yygh.hospital.service.ApiService;
+import com.yygh.common.exception.YyghException;
+import com.yygh.common.utils.MD5;
 import com.yygh.hospital.util.BeanUtils;
 import com.yygh.hospital.util.HttpRequestHelper;
-import com.yygh.hospital.util.MD5;
-import com.yygh.hospital.util.YyghException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
@@ -26,8 +25,8 @@ import java.io.InputStream;
 import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -120,7 +119,6 @@ public class ApiServiceImpl implements ApiService {
 
         Map<String, Object> paramMap = new HashMap<>();
         paramMap.put("hoscode",this.getHoscode());
-        //paramMap.put("depcode",depcode);
         paramMap.put("page",pageNum);
         paramMap.put("limit",pageSize);
         paramMap.put("timestamp", HttpRequestHelper.getTimestamp());
@@ -194,7 +192,6 @@ public class ApiServiceImpl implements ApiService {
         Map<String, Object> result = new HashMap();
         Map<String, Object> paramMap = new HashMap<>();
         paramMap.put("hoscode",this.getHoscode());
-        //paramMap.put("depcode",depcode);
         paramMap.put("page",pageNum);
         paramMap.put("limit",pageSize);
         paramMap.put("timestamp", HttpRequestHelper.getTimestamp());
@@ -329,8 +326,7 @@ public class ApiServiceImpl implements ApiService {
                     "西院区乘车路线：68路到辟才胡同东口；更多乘车路线详见须知。\n" +
                     "\n";
             paramMap.put("route",route);
-            //logo
-            //paramMap.put("logoData", this.getImgStr(jsonObject.getString("picture")));
+            //logoData 图片上传略
 
             Map<String, Object> bookingRuleMap = new HashMap<>();
             bookingRuleMap.put("cycle",10);
@@ -381,29 +377,25 @@ public class ApiServiceImpl implements ApiService {
         ByteArrayOutputStream outStream = null;
         HttpURLConnection httpUrl = null;
         try {
-            url = new URL(imgUrl);
+            url = URI.create(imgUrl).toURL();
             httpUrl = (HttpURLConnection) url.openConnection();
-            httpUrl.connect();
-            httpUrl.getInputStream();
             is = httpUrl.getInputStream();
             outStream = new ByteArrayOutputStream();
-            //创建一个Buffer字符串
             byte[] buffer = new byte[1024];
-            //每次读取的字符串长度，如果为-1，代表全部读取完毕
-            int len = 0;
-            //使用一个输入流从buffer里把数据读取出来
+            int len;
             while ((len = is.read(buffer)) != -1) {
-                //用输出流往buffer里写入数据，中间参数代表从哪个位置开始读，len代表读取的长度
                 outStream.write(buffer, 0, len);
             }
-            // 对字节数组Base64编码
             return outStream.toByteArray();
+        } catch (IllegalArgumentException | MalformedURLException e) {
+            log.error("url出现异常，url:{}", imgUrl, e);
+            throw new YyghException("图片URL格式错误", 201);
         } catch (ConnectException e) {
-            log.error("获取图片时连接异常，url:{}",imgUrl,e);
-        } catch (MalformedURLException e) {
-            log.error("url出现异常，url:{}",imgUrl,e);
+            log.error("获取图片时连接异常，url:{}", imgUrl, e);
+            throw new YyghException("获取图片连接失败", 201);
         } catch (IOException e) {
-            log.error("读取图片时发生异常，url:{}",imgUrl,e);
+            log.error("读取图片时发生异常，url:{}", imgUrl, e);
+            throw new YyghException("读取图片失败", 201);
         } finally {
             if (is != null) {
                 try {
@@ -423,6 +415,5 @@ public class ApiServiceImpl implements ApiService {
                 httpUrl.disconnect();
             }
         }
-        return null;
     }
 }
