545fbc5aef8e7e0d01f369fae1c258db4a06206d
xxinj <xxinjie21@163.com>
2026-07-04 01:18:24 +0800

    补充缺失sql建表

--- sql/yygh_hosp.sql
+++ sql/yygh_hosp.sql
@@ -45,4 +45,48 @@ INSERT INTO `hospital_set` VALUES (2, '北京妇产医院', '1000_01', 'http://l
 INSERT INTO `hospital_set` VALUES (3, '华西口腔医院', '1000_2', 'http://localhost:9999', 'bafd25a47089ea051737feb989be0dbd', '小熊熊', '123321', 0, '2022-07-04 10:38:30', '2022-07-04 10:38:30', 0);
 INSERT INTO `hospital_set` VALUES (4, '武汉同济医院', '1000_3', '333', '8d56f8e337417a5b179068f59ca7d4c6', '1332', '133', 1, '2022-07-04 11:06:36', '2022-07-04 11:08:29', 0);
 
+-- ----------------------------
+-- 表结构: hospital
+-- ----------------------------
+DROP TABLE IF EXISTS `hospital`;
+CREATE TABLE `hospital`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `hoscode` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '医院编号',
+  `hosname` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '医院名称',
+  `hostype` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '医院类型',
+  `province_code` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '省code',
+  `city_code` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '市code',
+  `district_code` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '区code',
+  `address` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '详情地址',
+  `logo_data` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '医院logo',
+  `intro` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '医院简介',
+  `route` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '坐车路线',
+  `status` tinyint NOT NULL DEFAULT 0 COMMENT '状态 0：未上线 1：已上线',
+  `booking_rule` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '预约规则(JSON)',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `is_deleted` tinyint NOT NULL DEFAULT 0 COMMENT '逻辑删除(1:已删除，0:未删除)',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `uk_hoscode`(`hoscode` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '医院信息表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- 表结构: department
+-- ----------------------------
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE `department`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `hoscode` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '医院编号',
+  `depcode` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '科室编号',
+  `depname` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '科室名称',
+  `intro` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '科室描述',
+  `bigcode` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '大科室编号',
+  `bigname` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '大科室名称',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `is_deleted` tinyint NOT NULL DEFAULT 0 COMMENT '逻辑删除(1:已删除，0:未删除)',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `uk_hoscode_depcode`(`hoscode` ASC, `depcode` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '科室信息表' ROW_FORMAT = Dynamic;
+
 SET FOREIGN_KEY_CHECKS = 1;
